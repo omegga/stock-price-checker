94a93b6b196e73bd3c2ca51184a49618cb5f0c89
omegga <whymagnet@gmail.com>
2019-04-26 06:22:32 +0200

    add dotenv

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ var express     = require('express');
 var bodyParser  = require('body-parser');
 var expect      = require('chai').expect;
 var cors        = require('cors');
+const dotenv = require('dotenv');
 
 var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
 var runner            = require('./test-runner');
 
+dotenv.config();
 var app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
